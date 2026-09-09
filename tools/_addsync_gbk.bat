c57f8d312dfb5c3e51fd62b0c5f41d40eb14a632
canaanser <632106943@qq.com>
2026-09-10 04:32:25 +0800

    review(baseline-cleanup): 移出探针/内嵌py310/敏感凭据与运行产物出版本跟踪(不物理删, 补 .gitignore)

--- tools/_addsync_gbk.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-schtasks /create /tn QuantDataSync /tr "\"D:\a数据\stockdb\数据更新.exe\"" /sc daily /st 20:00 /f
--- tools/_psq.ps1
+++
@@ -1,1 +0,0 @@
-Get-CimInstance Win32_Process | Where-Object { $_.Name -like '*看守*' } | ForEach-Object { Write-Output ($_.ProcessId.ToString() + " :: " + $_.CommandLine) }
